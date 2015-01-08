b8eb584e1e2eda985aa0ac146ee0ba5b1c784498
kszczepaniak <szczepaniak.krzysiek@gmail.com>
2015-01-08 18:06:49 +0100

    Version 8-01-2015
    działająca wersja poprzedniego

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,14 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameControl : MonoBehaviour {
 
 	public static GameControl control;
-	public float last_x = 0f;
-	public float last_y = 0f;
-	public string flag  = "";
-	public int health   = 100;
-	public int glucose  = 0;
+	public float last_x      = 0f;
+	public float last_y      = 0f;
+	public string flag       = "";
+	public string base_level = "";
+	public int health        = 100;
+	public int glucose       = 0;
+
+	public Rigidbody2D prefab_glucose;
+	
+	public List<List<float>> chamber1 = new List<List<float>> ();
+	public List<List<float>> chamber2 = new List<List<float>> ();
+	public List<List<float>> chamber3 = new List<List<float>> ();
 
 	// Use this for initialization
 	void Awake () {
@@ -20,6 +28,39 @@ public class GameControl : MonoBehaviour {
 		else if (control != this) {
 			Destroy(gameObject);
 		}
+
+		// initialize previous position of game elements
+		if (Application.loadedLevelName.Contains ("ch1") && control.chamber1.Count != 0) {
+			GameObject[] glucoses = GameObject.FindGameObjectsWithTag("Glucose");
+			foreach (GameObject glc in glucoses) {
+				Destroy(glc);
+			}
+			foreach(List<float> coords in control.chamber1) {
+				Instantiate(prefab_glucose, new Vector2(coords[0], coords[1]), Quaternion.identity);
+			}
+		}
+		else if (Application.loadedLevelName.Contains ("ch2") && control.chamber2.Count != 0) {
+			GameObject[] glucoses = GameObject.FindGameObjectsWithTag("Glucose");
+			foreach (GameObject glc in glucoses) {
+				Destroy(glc);
+			}
+			foreach(List<float> coords in control.chamber2) {
+				Instantiate(prefab_glucose, new Vector2(coords[0], coords[1]), Quaternion.identity);
+			}
+		}
+		else if (Application.loadedLevelName.Contains ("ch3") && control.chamber3.Count != 0) {
+			GameObject[] glucoses = GameObject.FindGameObjectsWithTag("Glucose");
+			foreach (GameObject glc in glucoses) {
+				Destroy(glc);
+			}
+			foreach(List<float> coords in control.chamber3) {
+				Instantiate(prefab_glucose, new Vector2(coords[0], coords[1]), Quaternion.identity);
+			}
+		}
+	}
+
+	void Start() {
+
 	}
 	
 	// Update is called once per frame
@@ -29,5 +70,6 @@ public class GameControl : MonoBehaviour {
 	void OnGUI() {
 		GUI.Label (new Rect (10, 10, 100, 30), "Health: " + health);
 		GUI.Label (new Rect (10, 25, 100, 30), "Glucose: " + glucose);
+		GUI.Label (new Rect (10, 40, 100, 30), Application.loadedLevelName);
 	}
 }
--- Assets/Scripts/TeleportScript.cs
+++ Assets/Scripts/TeleportScript.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TeleportScript : MonoBehaviour {
 
 	public string level = "";
+	public string main_chamber = "";
+	public float delta_y = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -15,11 +18,26 @@ public class TeleportScript : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D bacteria) {
 		if(bacteria.gameObject.name == "player"){
-			if (Application.loadedLevelName == "chamber1") {
+			if (Application.loadedLevelName == main_chamber) {
+				// saving position of the player
 				GameControl.control.last_x = bacteria.gameObject.rigidbody2D.position.x;
-				GameControl.control.last_y = bacteria.gameObject.rigidbody2D.position.y - 0.5f;
+				GameControl.control.last_y = bacteria.gameObject.rigidbody2D.position.y - delta_y;
 				GameControl.control.flag   = "return";
+				GameControl.control.base_level = main_chamber;
 			}
+
+			// saving position of game objects depending on current level
+			GameObject[] glucoses = GameObject.FindGameObjectsWithTag("Glucose");
+			if(Application.loadedLevelName.Contains("ch1")) {
+				GameControl.control.chamber1.Clear();
+				foreach (GameObject glc in glucoses) { GameControl.control.chamber1.Add(new List<float>{glc.gameObject.rigidbody2D.position.x, glc.gameObject.rigidbody2D.position.y}); }}
+			if(Application.loadedLevelName.Contains("ch2")) {
+				GameControl.control.chamber2.Clear();
+				foreach (GameObject glc in glucoses) { GameControl.control.chamber2.Add(new List<float>{glc.gameObject.rigidbody2D.position.x, glc.gameObject.rigidbody2D.position.y}); }}
+			if(Application.loadedLevelName.Contains("ch3")) {
+				GameControl.control.chamber3.Clear();
+				foreach (GameObject glc in glucoses) { GameControl.control.chamber3.Add(new List<float>{glc.gameObject.rigidbody2D.position.x, glc.gameObject.rigidbody2D.position.y}); }}
+
 			Application.LoadLevel (level);
 		}
 	}
