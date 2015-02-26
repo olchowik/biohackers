4c31b4fb8551fb1c10aa28b47b1b751f819ab1ed
kszczepaniak <szczepaniak.krzysiek@gmail.com>
2015-02-26 11:43:17 +0100

    version 26-02
    added GUI prototype

---
+++ Assets/LifeBarScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LifeBarScript : MonoBehaviour {
+
+	private Image lifebar;
+
+	// Use this for initialization
+	void Start () {
+		//Image lifebar = gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
